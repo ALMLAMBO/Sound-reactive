40b23dc2658d5a7216c9608be86372dd18e4c3b1
ALMLAMBO <aleksm007148@gmail.com>
2020-04-12 23:16:24 +0300

    Update file

--- embedded/server/server.ino
+++ embedded/server/server.ino
@@ -1,9 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP8266WiFi.h>
 
+const char * ssid = "sound_reactive";
+const char * password = "password";
+
+WiFiServer server(80);
+String header;
+
+
+void setup() {
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
