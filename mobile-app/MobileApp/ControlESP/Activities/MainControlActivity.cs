2233faf5647372a098036851960b042d24bd99d7
ALMLAMBO <aleksm007148@gmail.com>
2020-04-12 23:59:32 +0300

    Add activity for controlling the system

---
+++ mobile-app/MobileApp/ControlESP/Activities/MainControlActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Essentials;
+
+namespace ControlESP.Activities {
+	[Activity(Label = "MainControlActivity", Theme = "@style/AppTheme")]
+	public class MainControlActivity : AppCompatActivity {
+		protected override void OnCreate(Bundle savedInstanceState) {
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.main_control_activity);
+		}
+	}
+}
