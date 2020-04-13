76582059b8dd4ae4ee56cd991f954432a9c24431
ALMLAMBO <aleksm007148@gmail.com>
2020-04-13 23:15:39 +0300

    Small fixes

--- embedded/server/server.ino
+++ embedded/server/server.ino
@@ -1,33 +1,48 @@
 #include <ESP8266WiFi.h>
 
-const char * ssid = "sound";
+const char * ssid = "sound_reactive";
 const char * password = "password";
 
-WiFiServer server(80);
+WiFiServer server(8080);
 String header;
 
 void setup() {
   Serial.begin(115200);
-  digitalWrite(LED_BUILTIN, LOW);
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  WiFi.begin(ssid, password);
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, password);
 
-  int i = 0;
-  while (WiFi.status() != WL_CONNECTED) {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  int watchDog = 0;
+  while (WiFi.status() != WL_DISCONNECTED) {
     delay(500);
-    Serial.print(++i); Serial.print(' ');
+    watchDog++;
+    
+    if(watchDog == 60) {
+      WiFi.disconnect();
+      WiFi.printDiag(Serial);
+      watchDog = 0;
+      return;
+    }
   }
 
-  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("Connected");
 
-  Serial.println("");
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.softAPIP());
   server.begin();
 }
 
 void loop() {
+  WiFiClient new_client = server.available();
+  if(new_client) {
+    Serial.println("New client connected");
+  }
   
+  delay(1000);
 }
