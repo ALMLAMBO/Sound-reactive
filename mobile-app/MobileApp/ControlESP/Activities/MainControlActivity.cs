22f66f4c986930b8e0001a8dfc25330cd5878eb4
ALMLAMBO <aleksm007148@gmail.com>
2020-04-13 23:14:40 +0300

    Add structure for sending commands

--- mobile-app/MobileApp/ControlESP/Activities/MainControlActivity.cs
+++ mobile-app/MobileApp/ControlESP/Activities/MainControlActivity.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 
 using Android.App;
@@ -19,6 +21,25 @@ namespace ControlESP.Activities {
 			base.OnCreate(savedInstanceState);
 
 			SetContentView(Resource.Layout.main_control_activity);
+
+			Button button = FindViewById<Button>(
+				Resource.Id.led_on_button);
+
+			button.Click += SendLedOn_Click;
+		}
+
+		private void SendLedOn_Click(object sender, EventArgs e) {
+			try {
+				new Android.App.AlertDialog.Builder(this)
+					.SetMessage("Sended command")
+					.Show();
+
+				return;
+			} 
+			catch (Exception) {
+
+				throw;
+			}
 		}
 	}
 }
